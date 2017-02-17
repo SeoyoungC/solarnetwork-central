7630b096f5855476becc1cc6ac3fe58a85b476f5
Matt Magoffin <matt@solarnetwork.net>
2017-02-17 17:43:52 +1300

    Add another test.

--- net.solarnetwork.central.datum/defs/sql/postgres/plv8/test/datum/datumAggregateTests.js
+++ net.solarnetwork.central.datum/defs/sql/postgres/plv8/test/datum/datumAggregateTests.js
@@ -31,8 +31,8 @@ function parseDatumCSV(filename) {
 }
 
 test('datum:datumAggregate:create', t => {
-	const service = datumAggregate('foo', 123, 234);
-	t.is(service.sourceId, 'foo');
+	const service = datumAggregate('Foo', 123, 234);
+	t.is(service.sourceId, 'Foo');
 	t.is(service.ts, 123);
 	t.is(service.endTs, 234);
 });
@@ -40,7 +40,7 @@ test('datum:datumAggregate:create', t => {
 test('datum:datumAggregate:processRecords:15m:1', t => {
 	const slotTs = 1476046800000;
 	const endTs = slotTs + (15 * 60 * 1000);
-	const sourceId = 'foo';
+	const sourceId = 'Foo';
 	const service = datumAggregate(sourceId, slotTs, endTs);
 	t.is(service.sourceId, sourceId);
 	t.is(service.ts, slotTs);
@@ -48,14 +48,6 @@ test('datum:datumAggregate:processRecords:15m:1', t => {
 
 	const data = parseDatumCSV('/find-datum-for-minute-time-slots-01.csv').slice(0, 5);
 
-/*
-"ts_start","source_id","jdata"
-"2016-10-10 09:45:00+13","Foo","{""i"":{""foo"":0},""a"":{""bar"":0}}"
-"2016-10-10 10:00:00+13","Foo","{""i"":{""foo"":1},""a"":{""bar"":5}}"
-"2016-10-10 10:00:00+13","Foo","{""i"":{""foo"":2},""a"":{""bar"":10}}"
-"2016-10-10 10:00:00+13","Foo","{""i"":{""foo"":3},""a"":{""bar"":20}}"
-"2016-10-10 10:15:00+13","Foo","{""i"":{""foo"":4},""a"":{""bar"":30}}"
-*/
 	var aggResult;
 	data.forEach(rec => {
 		if ( rec.ts_start.getTime() < endTs ) {
@@ -75,7 +67,7 @@ test('datum:datumAggregate:processRecords:15m:1', t => {
 test('datum:datumAggregate:processRecords:15m:trailingFraction', t => {
 	const slotTs = 1476050400000;
 	const endTs = slotTs + (15 * 60 * 1000);
-	const sourceId = 'foo';
+	const sourceId = 'Foo';
 	const service = datumAggregate(sourceId, slotTs, endTs);
 	t.is(service.sourceId, sourceId);
 	t.is(service.ts, slotTs);
@@ -83,16 +75,6 @@ test('datum:datumAggregate:processRecords:15m:trailingFraction', t => {
 
 	const data = parseDatumCSV('/find-datum-for-minute-time-slots-02.csv');
 
-/*
-"ts","ts_start","source_id","jdata"
-"2016-10-10 10:50:00+13","2016-10-10 10:45:00+13","Foo","{""i"":{""foo"":11},""a"":{""bar"":100}}"
-"2016-10-10 11:00:00+13","2016-10-10 11:00:00+13","Foo","{""i"":{""foo"":13},""a"":{""bar"":110}}"
-"2016-10-10 11:09:00+13","2016-10-10 11:00:00+13","Foo","{""i"":{""foo"":15},""a"":{""bar"":115}}"
-"2016-10-10 11:11:00+13","2016-10-10 11:00:00+13","Foo","{""i"":{""foo"":17},""a"":{""bar"":120}}"
-"2016-10-10 11:17:00+13","2016-10-10 11:15:00+13","Foo","{""i"":{""foo"":19},""a"":{""bar"":130}}"
-
-The last two rows split their accumulation; 2/3 attributed to this time slot (1/3 to next).
-*/
 	var aggResult;
 	data.forEach(rec => {
 		if ( rec.ts_start.getTime() < endTs ) {
@@ -112,7 +94,7 @@ The last two rows split their accumulation; 2/3 attributed to this time slot (1/
 test('datum:datumAggregate:processRecords:15m:leadingFraction', t => {
 	const slotTs = 1476050400000;
 	const endTs = slotTs + (15 * 60 * 1000);
-	const sourceId = 'foo';
+	const sourceId = 'Foo';
 	const service = datumAggregate(sourceId, slotTs, endTs);
 	t.is(service.sourceId, sourceId);
 	t.is(service.ts, slotTs);
@@ -120,16 +102,6 @@ test('datum:datumAggregate:processRecords:15m:leadingFraction', t => {
 
 	const data = parseDatumCSV('/find-datum-for-minute-time-slots-03.csv');
 
-/*
-"ts","ts_start","source_id","jdata"
-"2016-10-10 10:50:00+13","2016-10-10 10:45:00+13","Foo","{""i"":{""foo"":11},""a"":{""bar"":100}}"
-"2016-10-10 11:02:00+13","2016-10-10 11:00:00+13","Foo","{""i"":{""foo"":13},""a"":{""bar"":110}}"
-"2016-10-10 11:09:00+13","2016-10-10 11:00:00+13","Foo","{""i"":{""foo"":15},""a"":{""bar"":115}}"
-"2016-10-10 11:11:00+13","2016-10-10 11:00:00+13","Foo","{""i"":{""foo"":17},""a"":{""bar"":120}}"
-"2016-10-10 11:15:00+13","2016-10-10 11:15:00+13","Foo","{""i"":{""foo"":19},""a"":{""bar"":130}}"
-
-The first two rows split their accumulation; 1/6 attributed to this time slot (5/6 to previous).
-*/
 	var aggResult;
 	data.forEach(rec => {
 		if ( rec.ts_start.getTime() < endTs ) {
@@ -144,4 +116,31 @@ The first two rows split their accumulation; 1/6 attributed to this time slot (5
 
 	t.deepEqual(aggResult.jdata.i, {foo:15, foo_min:13, foo_max:17});
 	t.deepEqual(aggResult.jdata.a, {bar:21.667}, '1/6 of first record\'s accumulation counts towards this result');
+});
+
+test('datum:datumAggregate:processRecords:15m:leadingAndTrailingFractions', t => {
+	const slotTs = 1476050400000;
+	const endTs = slotTs + (15 * 60 * 1000);
+	const sourceId = 'Foo';
+	const service = datumAggregate(sourceId, slotTs, endTs);
+	t.is(service.sourceId, sourceId);
+	t.is(service.ts, slotTs);
+	t.is(service.endTs, endTs);
+
+	const data = parseDatumCSV('/find-datum-for-minute-time-slots-04.csv');
+
+	var aggResult;
+	data.forEach(rec => {
+		if ( rec.ts_start.getTime() < endTs ) {
+			service.addDatumRecord(rec);
+		} else {
+			aggResult = service.finish(rec);
+		}
+	});
+
+	t.is(aggResult.source_id, sourceId);
+	t.is(aggResult.ts_start.getTime(), slotTs);
+
+	t.deepEqual(aggResult.jdata.i, {foo:15, foo_min:13, foo_max:17});
+	t.deepEqual(aggResult.jdata.a, {bar:18.333}, '1/6 of first; 2/3 of last record\'s accumulation counts towards this result');
 });
