dd182211dbf43fd248d3728ddcf11ee3dfb479f9
Matt Magoffin <matt@solarnetwork.net>
2017-02-17 14:31:10 +1300

    Work more on datum aggregate object.

--- net.solarnetwork.central.datum/defs/sql/postgres/plv8/src/datum/datumAggregate.js
+++ net.solarnetwork.central.datum/defs/sql/postgres/plv8/src/datum/datumAggregate.js
@@ -1,3 +1,60 @@
+'use strict';
+
+import calculateAverages from '../math/calculateAverages.js'
+
+/**
+ * Add a number value to an object at a specific key, limited to a percentage value.
+ * Optionally increment a number "counter" value for the same key, if a counter
+ * object is provided. This is designed to support aggregation of values, both simple
+ * sum aggregates and average aggregates where the count will be later used to calculate
+ * an effective average of the computed sum.
+ *
+ * @param {String} k The object key.
+ * @param {Number} v The value to add.
+ * @param {Object} o The object to manipulate.
+ * @param {Number} p A percentage (0-1) to apply to the value before adding. If not defined
+ *                   then 1 is assumed.
+ * @param {Object} c An optional "counter" object, whose <code>k</code> property will be incremented
+ *                   by 1 if defined, or set to 1 if not.
+ * @param {Object} r An optional "stats" object, whose <code>k</code> property will be maintained
+ *                   as a nested object with <code>min</code> and <code>max</code> properties.
+ */
+function addTo(k, v, o, p, c, r) {
+	var newVal;
+	if ( p === undefined ) {
+		p = 1;
+	}
+	newVal = (v * p);
+	if ( o[k] === undefined ) {
+		o[k] = newVal;
+	} else {
+		o[k] += newVal;
+	}
+	if ( c ) {
+		if ( c[k] === undefined ) {
+			c[k] = 1;
+		} else {
+			c[k] += 1;
+		}
+	}
+	if ( r ) {
+		if ( r[k] === undefined ) {
+			r[k] = { min: newVal, max: newVal };
+		} else {
+			if ( r[k].min === undefined ) {
+				r[k].min = newVal;
+			} else if ( newVal < r[k].min ) {
+				r[k].min = newVal;
+			}
+			if ( r[k].max === undefined ) {
+				r[k].max = newVal;
+			} else if ( newVal > r[k].max ) {
+				r[k].max = newVal;
+			}
+		}
+	}
+}
+
 /**
  * An aggregate record object that helps keep track of the raw data needed to
  * calculate a single aggregate result from many input records.
@@ -15,7 +72,33 @@ export default function datumAggregate(sourceId, ts, endTs) {
 	var iobjCounts = {};
 	var iobjStats = {};
 	var sobj = {};
+	var tarr = [];
 	var accAvg = {};
+	var prevRecord;
+
+	function addInstantaneousValues(inst) {
+		var prop;
+		for ( prop in inst ) {
+			addTo(prop, inst[prop], iobj, 1, iobjCounts, iobjStats);
+		}
+	}
+
+	function addStaticValues(stat) {
+		var prop;
+		for ( prop in stat ) {
+			sobj[prop] = stat[prop];
+		}
+	}
+
+	function addTagValues(tags) {
+		var i, t;
+		for ( i = 0; i < tags.length; i += 1 ) {
+			t = tags[i];
+			if ( tarr.indexOf(t) === -1 ) {
+				tarr.push(t);
+			}
+		}
+	}
 
 	/**
 	 * Add another datum record.
@@ -25,6 +108,37 @@ export default function datumAggregate(sourceId, ts, endTs) {
 	 * @param {Object} record[jdata] The datum JSON data object.
 	 */
 	function addDatumRecord(record) {
+		if ( !(record && record.jdata) ) {
+			return;
+		}
+		var recTs = record.tsms,
+			inst = record.jdata.i,
+			stat = record.jdata.s,
+			tags = record.jdata.t;
+
+		// as long as this record's time slot falls within the configured time slot,
+		// handle instantaneous, static, and tag values
+		if ( recTs === ts ) {
+			if ( inst ) {
+				// add instantaneous values
+				addInstantaneousValues(inst);
+			}
+
+			// merge in static values
+			if ( stat ) {
+				addStaticValues(stat);
+			}
+
+			// add tag values
+			if ( Array.isArray(tags) ) {
+				addTagValues(tags);
+			}
+		}
+
+		// TODO: handle accumulating values
+
+		// save curr record as previous for future calculations
+		prevRecord = record;
 	}
 
 	/**
@@ -36,10 +150,45 @@ export default function datumAggregate(sourceId, ts, endTs) {
 	 */
 	function finish(nextRecord) {
 		var aggRecord = {
-			source_id : sourceId,
-			tsms      : ts,
-		};
+				ts_start  : new Date(ts),
+				source_id : sourceId,
+				jdata     : {},
+			},
+			prop,
+			aggInst;
+
+		// handle any fractional portion of the next record
+		if ( nextRecord ) {
+			addDatumRecord(nextRecord);
+		}
+
+		// calculate our instantaneous average values
+		aggInst = calculateAverages(iobj, iobjCounts);
+
+		// inject min/max statistic values for instantaneous average values
+		for ( prop in aggInst ) {
+			if ( aggRecord.jdata.i === undefined ) {
+				aggRecord.jdata.i = aggInst;
+			}
+			if ( iobjStats[prop] !== undefined ) {
+				if ( iobjStats[prop].min !== undefined && iobjStats[prop].min !== aggInst[prop] ) {
+					aggInst[prop+'_min'] = iobjStats[prop].min;
+				}
+				if ( iobjStats[prop].max !== undefined && iobjStats[prop].max !== aggInst[prop] ) {
+					aggInst[prop+'_max'] = iobjStats[prop].max;
+				}
+			}
+		}
+
+		// TODO add accumulating results via merge() to pick fixPrecision() values
+		//aggRecord.jdata.a = merge({}, aobj);
+
 		return aggRecord
+	}
+
+	function startNext(nextTs, nextEndTs) {
+		var result = datumAggregate(sourceId, nextTs, nextEndTs);
+
 	}
 
 	return Object.defineProperties(self, {
--- net.solarnetwork.central.datum/defs/sql/postgres/plv8/test/datum/datumAggregateTests.js
+++ net.solarnetwork.central.datum/defs/sql/postgres/plv8/test/datum/datumAggregateTests.js
@@ -1,23 +1,27 @@
 import test from 'ava';
-import csv from 'csv';
+import csvParse from 'csv-parse/lib/sync';
 import fs from 'fs';
+import moment from 'moment';
+
 import datumAggregate from '../../src/datum/datumAggregate'
 
-function parseDatumCSV(filename, callback) {
-	var records = [];
-	fs.createReadStream(__dirname+'/'+filename)
-		.pipe(csv.parse({
+function parseDatumCSV(filename) {
+	var data = fs.readFileSync(__dirname+'/'+filename, { encoding : 'utf8' });
+	var records = csvParse(data, {
 			auto_parse : true,
 			columns : true,
-		}))
-		.pipe(csv.transform(function(record) {
-			if ( record.jdata ) {
-				record.jdata = JSON.parse(record.jdata);
-			}
-			records.push(record);
-		})).on('finish', function() {
-			callback(null, records);
 		});
+	var i, record;
+	for ( i = 0; i < records.length; i+= 1 ) {
+		record = records[i];
+		if ( record.jdata ) {
+			record.jdata = JSON.parse(record.jdata);
+		}
+		if ( record.ts ) {
+			record.ts = moment(record.ts).toDate();
+		}
+	}
+	return records;
 }
 
 test('datum:datumAggregate:create', t => {
@@ -27,14 +31,36 @@ test('datum:datumAggregate:create', t => {
 	t.is(service.endTs, 234);
 });
 
-test.cb('datum:datumAggregate:processRecords:15m:1', t => {
-	const service = datumAggregate('foo', 1476046800000, 1476047700000);
-	t.is(service.sourceId, 'foo');
-	t.is(service.ts, 1476046800000);
+test('datum:datumAggregate:processRecords:15m:1', t => {
+	const slotTs = 1476046800000;
+	const sourceId = 'foo';
+	const service = datumAggregate(sourceId, slotTs, 1476047700000);
+	t.is(service.sourceId, sourceId);
+	t.is(service.ts, slotTs);
 	t.is(service.endTs, 1476047700000);
 
-	parseDatumCSV('/find-datum-for-minute-time-slots-01.csv', function(error, records) {
-		console.log('Got records: ', records);
-		t.end();
+	const data = parseDatumCSV('/find-datum-for-minute-time-slots-01.csv').slice(0, 5);
+
+/*
+"ts_start","source_id","tsms","percent","jdata"
+"2016-10-10 09:45:00+13","Foo",1476045900000,0,"{""i"":{""foo"":0},""a"":{""bar"":0}}"
+"2016-10-10 10:00:00+13","Foo",1476046800000,0,"{""i"":{""foo"":1},""a"":{""bar"":0}}"
+"2016-10-10 10:00:00+13","Foo",1476046800000,1,"{""i"":{""foo"":2},""a"":{""bar"":10}}"
+"2016-10-10 10:00:00+13","Foo",1476046800000,1,"{""i"":{""foo"":3},""a"":{""bar"":20}}"
+"2016-10-10 10:15:00+13","Foo",1476047700000,0,"{""i"":{""foo"":4},""a"":{""bar"":30}}"
+*/
+	var aggResult;
+	data.forEach(rec => {
+		if ( rec.tsms < 1476047700000 ) {
+			service.addDatumRecord(rec);
+		} else {
+			aggResult = service.finish(rec);
+		}
 	});
+
+	t.is(aggResult.source_id, sourceId);
+	t.is(aggResult.ts_start.getTime(), slotTs);
+
+	t.deepEqual(aggResult.jdata.i, {foo:2, foo_min:1, foo_max:3});
+
 });
