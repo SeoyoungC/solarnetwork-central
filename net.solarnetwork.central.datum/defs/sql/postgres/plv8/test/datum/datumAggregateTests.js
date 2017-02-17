f583f54d2286c195ec4bf1f18de186514a0b4bfa
Matt Magoffin <matt@solarnetwork.net>
2017-02-18 09:12:14 +1300

    Split utility helper into own file to share with other tests.

--- net.solarnetwork.central.datum/defs/sql/postgres/plv8/test/datum/datumAggregateTests.js
+++ net.solarnetwork.central.datum/defs/sql/postgres/plv8/test/datum/datumAggregateTests.js
@@ -1,35 +1,8 @@
 import test from 'ava';
-import csvParse from 'csv-parse/lib/sync';
-import fs from 'fs';
 import moment from 'moment';
 
-import datumAggregate from '../../src/datum/datumAggregate'
-
-function parseDatumCSV(filename) {
-	var data = fs.readFileSync(__dirname+'/'+filename, { encoding : 'utf8' });
-	var records = csvParse(data, {
-			auto_parse : true,
-			columns : true,
-			comment : '#',
-		});
-	var i, record;
-	for ( i = 0; i < records.length; i+= 1 ) {
-		record = records[i];
-		// convert ts into actual Date object
-		if ( record.ts ) {
-			record.ts = moment(record.ts).toDate();
-		}
-		// convert ts_start into actual Date object
-		if ( record.ts_start ) {
-			record.ts_start = moment(record.ts_start).toDate();
-		}
-		// convert jdata into JSON object
-		if ( record.jdata ) {
-			record.jdata = JSON.parse(record.jdata);
-		}
-	}
-	return records;
-}
+import parseDatumCSV from './_parseDatumCSV.js'
+import datumAggregate from '../../src/datum/datumAggregate.js'
 
 test('datum:datumAggregate:create', t => {
 	const service = datumAggregate('Foo', 123, 234);
