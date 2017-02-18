1fea7611b73aeff9d59069fa2d53389b9e00d481
Matt Magoffin <matt@solarnetwork.net>
2017-02-19 07:51:44 +1300

    Move addTo into stand-alone function and add unit tests.

--- net.solarnetwork.central.datum/defs/sql/postgres/plv8/src/datum/datumAggregate.js
+++ net.solarnetwork.central.datum/defs/sql/postgres/plv8/src/datum/datumAggregate.js
@@ -1,60 +1,8 @@
 'use strict';
 
 import calculateAverages from '../math/calculateAverages.js'
+import addTo from '../util/addTo.js'
 import mergeObjects from '../util/mergeObjects.js'
-
-/**
- * Add a number value to an object at a specific key, limited to a percentage value.
- * Optionally increment a number "counter" value for the same key, if a counter
- * object is provided. This is designed to support aggregation of values, both simple
- * sum aggregates and average aggregates where the count will be later used to calculate
- * an effective average of the computed sum.
- *
- * @param {String} k The object key.
- * @param {Number} v The value to add.
- * @param {Object} o The object to manipulate.
- * @param {Number} p A percentage (0-1) to apply to the value before adding. If not defined
- *                   then 1 is assumed.
- * @param {Object} c An optional "counter" object, whose <code>k</code> property will be incremented
- *                   by 1 if defined, or set to 1 if not.
- * @param {Object} r An optional "stats" object, whose <code>k</code> property will be maintained
- *                   as a nested object with <code>min</code> and <code>max</code> properties.
- */
-function addTo(k, v, o, p, c, r) {
-	var newVal;
-	if ( p === undefined ) {
-		p = 1;
-	}
-	newVal = (v * p);
-	if ( o[k] === undefined ) {
-		o[k] = newVal;
-	} else {
-		o[k] += newVal;
-	}
-	if ( c ) {
-		if ( c[k] === undefined ) {
-			c[k] = 1;
-		} else {
-			c[k] += 1;
-		}
-	}
-	if ( r ) {
-		if ( r[k] === undefined ) {
-			r[k] = { min: newVal, max: newVal };
-		} else {
-			if ( r[k].min === undefined ) {
-				r[k].min = newVal;
-			} else if ( newVal < r[k].min ) {
-				r[k].min = newVal;
-			}
-			if ( r[k].max === undefined ) {
-				r[k].max = newVal;
-			} else if ( newVal > r[k].max ) {
-				r[k].max = newVal;
-			}
-		}
-	}
-}
 
 /**
  * An aggregate record object that helps keep track of the raw data needed to
@@ -63,14 +11,26 @@ function addTo(k, v, o, p, c, r) {
  * @param {String} sourceId    The source ID.
  * @param {Number} ts          The timestamp associated with this aggregate result (e.g. time slot).
  * @param {Number} endTs       The timestamp (exclusive) of the end of this aggregate result (e.g. next time slot).
- * @param {Number} toleranceMs The number of milliseconds tolerance before/after time slot to allow calculating
- *                             accumulating values from. Defaults to 3600000.
+ *
+ * @param {Object} configuration An optional set of configuration properties.
+ * @param {Number} configuration.toleranceMs The number of milliseconds tolerance before/after
+ *                                           time slot to allow calculating accumulating values
+ *                                           from. Defaults to 3600000.
+ * @param {Object} configuration.hourFill    An object whose keys represent instantaneous datum
+ *                                           properties that should used to derive accumulating
+ *                                           values named for the associated property value.
  */
-export default function datumAggregate(sourceId, ts, endTs, toleranceMs) {
+export default function datumAggregate(sourceId, ts, endTs, configuration) {
 	var self = {
 		version : '1'
 	};
-	var tolMs = (toleranceMs !== undefined ? toleranceMs : 3600000);
+
+	/** The number of milliseconds tolerance before/after time span to allow finding prev/next records. */
+	var toleranceMs = (configuration && configuration.toleranceMs !== undefined ? configuration.toleranceMs : 3600000);
+
+	/** A mapping of instantaneous datum property keys to associated accumulating property keys that should be derived.  */
+	var hourFill = 	(configuration && configuration.hourFill !== undefined ? configuration.hourFill : {'watts' : 'wattHours'});
+
 	var aobj = {};
 	var iobj = {};
 	var iobjCounts = {};
@@ -80,6 +40,27 @@ export default function datumAggregate(sourceId, ts, endTs, toleranceMs) {
 	var accAvg = {};
 	var prevRecord;
 	var finishRecord;
+
+	/**
+	 * Toggle between nothern/southern hemisphere seasons, or get the current setting.
+	 *
+	 * @param {boolean} [value] <em>true</em> for northern hemisphere seasons, <em>false</em> for sothern hemisphere
+	 * @returns when used as a getter, the current setting
+	 * @memberOf sn.chart.baseGroupedSeasonalLineChart
+	 */
+	function setHourFills(value) {
+		if ( !arguments.length ) return hourFill;
+		if ( value === northernHemisphere ) {
+			return parent.me;
+		}
+		northernHemisphere = (value === true);
+
+		// immediately update path colors
+		parent.svgDataRoot.selectAll('g.season').transition().duration(parent.transitionMs())
+			.style('stroke', seasonColorFn);
+
+		return parent.me;
+	};
 
 	function addInstantaneousValues(inst) {
 		var prop;
@@ -216,7 +197,7 @@ export default function datumAggregate(sourceId, ts, endTs, toleranceMs) {
 		// handle any fractional portion of the next record
 		if ( nextRecord ) {
 			// only add if the next record is not too far into the future from the last record
-			if ( prevRecord && nextRecord.ts.getTime() < prevRecord.ts.getTime() + tolMs ) {
+			if ( prevRecord && nextRecord.ts.getTime() < prevRecord.ts.getTime() + toleranceMs ) {
 				// calling addDatumRecord will update prevRecord, so keep a reference to the current value
 				finishRecord = prevRecord;
 				addDatumRecord(nextRecord);
@@ -263,7 +244,7 @@ export default function datumAggregate(sourceId, ts, endTs, toleranceMs) {
 	 * @returns {Object} A new <code>datumAggregate</code> object.
 	 */
 	function startNext(nextTs, nextEndTs) {
-		var result = datumAggregate(sourceId, nextTs, nextEndTs, tolMs);
+		var result = datumAggregate(sourceId, nextTs, nextEndTs, toleranceMs);
 		if ( finishRecord ) {
 			if ( prevRecord ) {
 				result.addDatumRecord(prevRecord);
@@ -277,7 +258,8 @@ export default function datumAggregate(sourceId, ts, endTs, toleranceMs) {
 		sourceId			: { value : sourceId },
 		ts					: { value : ts },
 		endTs				: { value : endTs },
-		toleranceMs			: { value : tolMs },
+		toleranceMs			: { value : toleranceMs },
+		hourFill            : { value : hourFill },
 
 		addDatumRecord		: { value : addDatumRecord },
 		finish				: { value : finish },
