3e09965efa7101356c999b9fe1d69db574984a60
Matt Magoffin <matt@solarnetwork.net>
2017-02-19 20:12:27 +1300

    Remove unused package file.

--- net.solarnetwork.central.datum/defs/sql/postgres/plv8/src/math/math.js
+++
@@ -1,9 +0,0 @@
-import calculateAverageOverHours from './calculateAverageOverHours.js';
-import calculateAverages from './calculateAverages.js';
-import fixPrecision from './fixPrecision.js';
-
-export default {
-	calculateAverageOverHours,
-	calculateAverages,
-	fixPrecision,
-};
