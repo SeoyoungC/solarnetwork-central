1e19ae5bdbe50a951c0f5dee4e71ad21c88e8b86
Matt Magoffin <matt@solarnetwork.net>
2017-02-20 17:36:53 +1300

    Fix package, add generated loader SQL.

---
+++ net.solarnetwork.central.datum/defs/sql/postgres/plv8/postgres-init-plv8-modules.sql
@@ -0,0 +1,11 @@
+\i ./dist/datum/aggregator.sql
+\i ./dist/datum/datumAggregate.sql
+\i ./dist/datum/logLocationAggregateMessage.sql
+\i ./dist/datum/logNodeAggregateMessage.sql
+\i ./dist/datum/slotAggregator.sql
+\i ./dist/math/calculateAverageOverHours.sql
+\i ./dist/math/calculateAverages.sql
+\i ./dist/math/fixPrecision.sql
+\i ./dist/util/addTo.sql
+\i ./dist/util/intervalMs.sql
+\i ./dist/util/mergeObjects.sql
